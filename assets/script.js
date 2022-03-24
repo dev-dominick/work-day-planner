bedabf97026b637e755d2e8d223e51387d0e3fba
Dom <Domalbano35@gmail.com>
2022-03-24 15:20:54 -0400

    building logic for displaying saved message

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,43 @@ $("#currentDay").text(today.format("MMM Do, YYYY hh:mm a"));
 
 
 $(".saveBtn").on("click", function () {
-  let text = $(".past").text().val()
-  console.log(text);
-
-  localStorage.setItem("toDo", JSON.stringify({text}))
+  let text = $(this).siblings(".description").val().trim()
+  let time = $(this).parent().attr("id")
+  // $(event.target).sibling(1).val()
+  localStorage.setItem(time, text)
 })
 
 
+localStorage.getItem("9")
+
+//add id to text areas
+//select text areas with jquery
+//target value with val method
+//assign localstorage.getitem to that value
+$(timeBlock9).val(localStorage.getItem("9"));
+
+
+// $("#9 .description").val(localStorage.getItem("9"));
+// $("#10 .description").val(localStorage.getItem("9"));
+// $("11 .description").val(localStorage.getItem("9"));
+// $("#12 .description").val(localStorage.getItem("9"));
+// $("#13 .description").val(localStorage.getItem("9"));
+// $("#14 .description").val(localStorage.getItem("9"));
+// $("#15 .description").val(localStorage.getItem("9"));
+// $("#16 .description").val(localStorage.getItem("9"));
+// $("#17 .description").val(localStorage.getItem("9"));
 
 
 
 
+
+
+
+if ($(this) === m) {
+  
+} else {
+  
+}
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
